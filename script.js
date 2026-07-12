a7ce736de3a1211da27460e7e45df3aafcbeb338
moumenmohammed <121378228+moumenmohammed@users.noreply.github.com>
2026-07-12 13:03:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -299,7 +299,7 @@ if (adminLoginForm) {
         const password = adminPasswordInput.value.trim();
         adminPasswordInput.value = "";
 
-        if (password === "Amino") {
+        if (password === "M0100#") {
             isAdminLoggedIn = true;
             adminLoginForm.classList.add("hidden");
             if (addProductForm) addProductForm.classList.remove("hidden");
